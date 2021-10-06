4a1e550e8001eeb6d47461acaa260ad860568fb3
IR_Kovshar <ikovshar91@gmail.com>
2021-10-06 17:00:39 +0600

    commit#3

--- src/main/java/com/example/sweater/config/WebSecurityConfig.java
+++ src/main/java/com/example/sweater/config/WebSecurityConfig.java
@@ -21,7 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/registration").permitAll()
+                    .antMatchers("/", "/registration", "/static/**","/img/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
